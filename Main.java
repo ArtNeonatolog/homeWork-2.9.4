efaff27593521debacb499a602bd98996d7e4c4c
Артем <marcus201@mail.ru>
2022-11-27 23:58:38 +0500

    Задание 2 (часть 1) выполнено

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        NewMap newMap = new NewMap();
+        System.out.println(newMap);
+        System.out.println();
+        System.out.println(newMap.transformedMap());
+
+
     }
 }
